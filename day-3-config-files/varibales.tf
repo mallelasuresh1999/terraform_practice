3490461c8413415cd611141fcfea24dbb9002fd0
Mallela Suresh <103832901+mallelasuresh1999@users.noreply.github.com>
2026-03-31 12:26:51 +0530

    day-3-config-files

---
+++ day-3-config-files/varibales.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+    description = "This is the ami_id"
+    type = string
+    default = ""
+}
+
+variable "instance_type" {
+    description = "This is the instance type"
+    type = string
+    default = ""
+}
